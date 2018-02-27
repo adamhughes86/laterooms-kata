28d405dca51ebd158893a47a9882c6a3b690aa59
Adam Hughes <adam@forwardtheory.co.uk>
2018-02-27 16:58:08 +0000

    Add hotel card basic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DataService } from './services/data.service';
 import { HeroComponent } from './components/hero/hero.component';
+import { HotelCardComponent } from './components/hotel-card/hotel-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent
+    HeroComponent,
+    HotelCardComponent
   ],
   imports: [
     BrowserModule
